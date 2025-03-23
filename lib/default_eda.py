c08b12c70ead2c5d447ce7e7675955db5b0e3a61
jintonic3561 <w.micropterus.kate25@gmail.com>
2025-03-23 22:22:18 +0900

    修正するもうまくいかず

--- lib/default_eda.py
+++ lib/default_eda.py
@@ -1,7 +1,13 @@
-from lib.tools import get_stats, load_data, print_json  # Don't edit this line
+from lib.tools import load_data
 
 df = load_data()
-df = get_stats(df)
+# Don't edit anything above this line!
+#################
+
+#############
+# Your code here
+stats = df.describe()
+#############
 
 # Finally, always call the print_json function on the analyzed data.
-print_json(df)
+print(stats.to_json())
--- lib/tools.py
+++ lib/tools.py
@@ -4,10 +4,6 @@ import os
 import pandas as pd
 
 
-def print_json(df: pd.DataFrame):
-    print(df.to_json())
-
-
 def load_data(dirctory="/work/data/input/"):
     files = glob.glob(os.path.join(dirctory, "*.csv")) + glob.glob(os.path.join(dirctory, "*.parquet"))
     assert len(files) > 0, "No files found in the input directory"
@@ -19,7 +15,3 @@ def load_data(dirctory="/work/data/input/"):
         df = pd.read_parquet(file_name)
 
     return df
-
-
-def get_stats(df: pd.DataFrame):
-    return df.describe()
